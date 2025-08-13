8af5fac2ece31e7aa06af096e8926f17104d0a64
ogabek_norpulatov <ogabeknorpulatov571@gmail.com>
2025-08-13 08:56:01 +0500

    crossOrigin

--- src/main/java/com/example/LMS/controller/CourseController.java
+++ src/main/java/com/example/LMS/controller/CourseController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/courses")
 public class CourseController {
     private final CourseService courseService;
--- src/main/java/com/example/LMS/controller/TeacherController.java
+++ src/main/java/com/example/LMS/controller/TeacherController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/teachers")
 public class TeacherController {
     private final TeacherService teacherService;
